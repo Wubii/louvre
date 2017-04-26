bd3f351ea4fc616dce189d6edc6612dc76496f6b
BATTISTA Marion <battista.marion@gmail;com>
2017-04-26 18:12:36 +0200

    add the dashboard controller

---
+++ src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        return $this->render('default/dashboard.html.twig');
+    }
+}
